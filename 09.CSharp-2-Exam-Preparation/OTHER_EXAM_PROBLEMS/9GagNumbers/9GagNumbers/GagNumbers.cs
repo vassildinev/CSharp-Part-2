7cf78e2327cd146e338a0538ec99446ef51c4edf
Васил Динев <vassildinev@gmail.com>
2015-03-03 09:42:08 +0200

    More problems solved.

---
+++ 09.CSharp-2-Exam-Preparation/OTHER_EXAM_PROBLEMS/9GagNumbers/9GagNumbers/GagNumbers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+class GagNumbers
+{
+    static string[] digits = { "-!", "**", "!!!", "&&", "&-", "!-", "*!!!", "&*!", "!!**!-" };
+    static void Main()
+    {
+        //INPUT
+        string number = Console.ReadLine();
+
+        //SOLUTION
+        string baseNineNumber = ConvertTextToDigitsNumberSystems(number, digits);
+        BigInteger result = Convert(baseNineNumber);
+
+        //OUTPUT
+        Console.WriteLine(result);
+    }
+
+    private static string ConvertTextToDigitsNumberSystems(string number, string[] digits)
+    {
+        string baseNineNumber = string.Empty;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            for (int j = 0; j < digits.Length; j++)
+            {
+                string digit = digits[j];
+                int digitLength = digit.Length;
+                if (i + digitLength - 1 < number.Length)
+                {
+                    string substr = number.Substring(i, digitLength);
+                    if (substr == digit)
+                    {
+                        baseNineNumber += j;
+                        i += digitLength - 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return baseNineNumber;
+    }
+
+    private static BigInteger Convert(string baseNineNumber)
+    {
+        BigInteger number = BigInteger.Parse(baseNineNumber);
+        if (number == 0)
+        {
+            return 0;
+        }
+        BigInteger output = 0;
+        int power = 0;
+        while (number!=0)
+        {
+            int digit = (int)(number % 10);
+            output += digit * Power(9, power);
+            number /= 10;
+            power++;
+        }
+        return output;
+    }
+
+    private static BigInteger Power(int p, int power)
+    {
+        BigInteger result = 1;
+        for (int i = 0; i < power; i++)
+        {
+            result *= p;
+        }
+        return result;
+    }
+}
---
+++ 09.CSharp-2-Exam-Preparation/OTHER_EXAM_PROBLEMS/SpecialValue/SpecialValue/SpecialValue.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class SpecialValue
+{
+    static void Main()
+    {
+        //INPUT
+        var arrays = new List<int[]>();
+
+        int numberOfArrays = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfArrays; i++)
+        {
+            var currentArray = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            arrays.Add(currentArray);
+        }
+
+        //SOLUTION
+        long specialValue = 1;
+        long bestSpecialValue = long.MinValue;
+        int path = 0;
+
+        int bestLen = GetBestArrayLength(arrays);
+
+        bool[,] visited = new bool[arrays.Count, bestLen];
+
+        for (int i = 0; i < arrays[0].Length; i++)
+        {
+            int currentValue = arrays[0][i];
+            int nextValue = currentValue;
+            int row = 0;
+            while (true)
+            {
+                // check if path ends
+                if (nextValue < 0)
+                {
+                    path++;
+                    specialValue = path - nextValue;
+                    if (specialValue > bestSpecialValue)
+                    {
+                        bestSpecialValue = specialValue;
+                    }
+                    specialValue = 0;
+                    row = 0;
+                    path = 0;
+                    break;
+                }
+                else
+                {
+                    row++;
+                    if (row == arrays.Count)
+                    {
+                        row = 0;
+                    }
+                    path++;
+                    nextValue = arrays[row][currentValue];
+                    if (nextValue >= 0 && visited[row, nextValue])
+                    {
+                        specialValue = 0;
+                        path = 0;
+                        if (specialValue > bestSpecialValue)
+                        {
+                            bestSpecialValue = specialValue;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        if (nextValue >= 0)
+                        {
+                            visited[row, nextValue] = true;
+                        }
+                    }
+                    currentValue = nextValue;
+                    continue;
+                }
+            }
+        }
+
+        //OUPTUT
+        Console.WriteLine(bestSpecialValue);
+    }
+
+    static int GetBestArrayLength(List<int[]> list)
+    {
+        int bestLen = 0;
+        for (int i = 0; i < list.Count; i++)
+        {
+            int currentLen = list[i].Length;
+            if (currentLen > bestLen)
+            {
+                bestLen = currentLen;
+            }
+        }
+        return bestLen;
+    }
+}
